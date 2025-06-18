1c4dbc4c4fb9748ef1aedc75d4c457aeb4374bbb
Gabriel Vaccaro <166189389+vaccaro12334@users.noreply.github.com>
2025-06-18 13:45:59 -0500

    Update mainfsm.v

--- mainfsm.v
+++ mainfsm.v
@@ -1,26 +1,189 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/17/2025 05:03:35 PM
-// Design Name: 
-// Module Name: mainfsm
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module mainfsm (
+	clk,
+	reset,
+	Op,
+	Funct,
+	IRWrite,
+	AdrSrc,
+	ALUSrcA,
+	ALUSrcB,
+	ResultSrc,
+	NextPC,
+	RegW,
+	MemW,
+	Branch,
+	ALUOp
+);
+	input wire clk;
+	input wire reset;
+	input wire [1:0] Op;
+	input wire [5:0] Funct;
+	output wire IRWrite;
+	output wire AdrSrc;
+	output wire [1:0] ALUSrcA;
+	output wire [1:0] ALUSrcB;
+	output wire [1:0] ResultSrc;
+	output wire NextPC;
+	output wire RegW;
+	output wire MemW;
+	output wire Branch;
+	output wire ALUOp;
+	reg [3:0] state;
+	reg [3:0] nextstate;
+	reg [12:0] controls;
+	localparam [3:0] FETCH = 0;
+	localparam [3:0] BRANCH = 9;
+	localparam [3:0] DECODE = 1;
+	localparam [3:0] EXECUTEI = 7;
+	localparam [3:0] EXECUTER = 6;
+	localparam [3:0] MEMADR = 2;
+	localparam [3:0] UNKNOWN = 10;
 
+	// state register
+	always @(posedge clk or posedge reset)
+		if (reset)
+			state <= FETCH;
+		else
+			state <= nextstate;
+	
 
-module mainfsm(
+	// ADD CODE BELOW
+  	// Finish entering the next state logic below.  We've completed the 
+  	// first two states, FETCH and DECODE, for you.
 
-    );
+  	// next state logic
+	always @(*)
+		casex (state)
+			FETCH: nextstate = DECODE;
+			DECODE:
+				case (Op)
+					2'b00:
+						if (Funct[5])
+							nextstate = EXECUTEI;
+						else
+							nextstate = EXECUTER;
+					2'b01: nextstate = MEMADR;
+					2'b10: nextstate = BRANCH;
+					default: nextstate = UNKNOWN;
+				endcase
+			EXECUTER: nextstate = ALUWB;
+			EXECUTEI: nextstate = ALUWB;
+			ALUWB: nextstate = FETCH;
+			MEMADR: nextstate = (Funct[0]) ? MEMRD : MEMWR; 
+			MEMRD: nextstate = MEMWB;
+			MEMWB: nextstate = FETCH;
+			MEMWR: nextstate = FETCH;
+			BRANCH: nextstate = FETCH;
+			default: nextstate = FETCH;
+		endcase
+
+	// ADD CODE BELOW
+	// Finish entering the output logic below.  We've entered the
+	// output logic for the first two states, FETCH and DECODE, for you.
+
+	// state-dependent output logic
+	always @(*)
+		case (state)
+			FETCH: controls = 13'b1000101001100;
+			DECODE: controls = 13'b0000001001100;
+			EXECUTER: controls = 13'b0000000001001; // ALU = A op B (R-type), A=R[Rs], B=R[Rd]
+			EXECUTEI: controls = 13'b0000000001101; // ALU = A op Imm
+			ALUWB:    controls = 13'b0001000000000; // Reg[rd] <- ALUResult
+			MEMADR:   controls = 13'b0000000001100; // ALU = A + offset (calc mem addr)
+			MEMRD:    controls = 13'b0000010100000; // MemRead (Adr from ALUResult), ResultSrc=01
+			MEMWB:    controls = 13'b0001000100000; // Reg[rd] <- MemData
+			MEMWR:    controls = 13'b0010010000000; // Mem[Adr] <- R[rt]
+			BRANCH:   controls = 13'b0100000010000; // PC <- PC+offset (B)
+			default:  controls = 13'bxxxxxxxxxxxxx;
+		endcase
+
+	assign {NextPC, Branch, MemW, RegW, IRWrite, AdrSrc, ResultSrc, ALUSrcA, ALUSrcB, ALUOp} = controls;
 endmodule
+
+module condlogic (
+	clk,
+	reset,
+	Cond,
+	ALUFlags,
+	FlagW,
+	PCS,
+	RegW,
+	MemW,
+	PCSrc,
+	RegWrite,
+	MemWrite
+);
+	input wire clk;
+	input wire reset;
+	input wire [3:0] Cond;
+	input wire [3:0] ALUFlags;
+	input wire [1:0] FlagW;
+	input wire PCS;
+	input wire RegW;
+	input wire MemW;
+	output wire PCSrc;
+	output wire RegWrite;
+	output wire MemWrite;
+	wire [1:0] FlagWrite;
+	wire [3:0] Flags;
+	wire CondEx;
+	flopenr #(2) flagreg1(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[1]),
+		.d(ALUFlags[3:2]),
+		.q(Flags[3:2])
+	);
+	flopenr #(2) flagreg0(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[0]),
+		.d(ALUFlags[1:0]),
+		.q(Flags[1:0])
+	);
+	condcheck cc(
+		.Cond(Cond),
+		.Flags(Flags),
+		.CondEx(CondEx)
+	);
+	assign FlagWrite = FlagW & {2 {CondEx}};
+	assign RegWrite = RegW & CondEx;
+	assign MemWrite = MemW & CondEx;
+	assign PCSrc = PCS & CondEx;
+endmodule
+module condcheck (
+	Cond,
+	Flags,
+	CondEx
+);
+	input wire [3:0] Cond;
+	input wire [3:0] Flags;
+	output reg CondEx;
+	wire neg;
+	wire zero;
+	wire carry;
+	wire overflow;
+	wire ge;
+	assign {neg, zero, carry, overflow} = Flags;
+	assign ge = neg == overflow;
+	always @(*)
+		case (Cond)
+			4'b0000: CondEx = zero;
+			4'b0001: CondEx = ~zero;
+			4'b0010: CondEx = carry;
+			4'b0011: CondEx = ~carry;
+			4'b0100: CondEx = neg;
+			4'b0101: CondEx = ~neg;
+			4'b0110: CondEx = overflow;
+			4'b0111: CondEx = ~overflow;
+			4'b1000: CondEx = carry & ~zero;
+			4'b1001: CondEx = ~(carry & ~zero);
+			4'b1010: CondEx = ge;
+			4'b1011: CondEx = ~ge;
+			4'b1100: CondEx = ~zero & ge;
+			4'b1101: CondEx = ~(~zero & ge);
+			4'b1110: CondEx = 1'b1;
+			default: CondEx = 1'bx;
+		endcase
+endmodule
+//Ambos modelos sacados de la semana 11
