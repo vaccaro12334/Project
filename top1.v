f948c9f6ab3d5914c7b60c7787b5737650d46080
Gabriel Vaccaro <166189389+vaccaro12334@users.noreply.github.com>
2025-06-18 13:27:50 -0500

    Update top.v

--- top1.v
+++ top1.v
@@ -1,21 +1,19 @@
-// top.v - Módulo principal del sistema ARM multicycle
-// Integra el procesador 'arm' y la memoria 'mem'
 
 module top (
-    input wire clk,             // Señal de reloj
-    input wire reset,           // Señal de reinicio
-    output wire [31:0] WriteData, // Datos a escribir en memoria
-    output wire [31:0] Adr,       // Dirección de acceso a memoria
-    output wire MemWrite          // Señal de escritura a memoria
+    input wire clk,             
+    input wire reset,           
+    output wire [31:0] WriteData, 
+    output wire [31:0] Adr,       
+    output wire MemWrite          
 );
 
-    // Cableado interno
+
     wire [31:0] PC;
     wire [31:0] Instr;
     wire [31:0] ReadData;
 
-    // Instancia del procesador ARM multicycle
-    // Este procesador ejecuta instrucciones incluyendo la nueva instrucción MUL
+
+    
     arm arm(
         .clk(clk),
         .reset(reset),
@@ -25,8 +23,8 @@ module top (
         .ReadData(ReadData)
     );
 
-    // Instancia de la memoria RAM compartida
-    // La memoria se conecta al procesador a través de WriteData, Adr, y MemWrite
+
+    // conecta al procesador a través de WriteData, Adr, y MemWrite
     mem mem(
         .clk(clk),
         .we(MemWrite),
